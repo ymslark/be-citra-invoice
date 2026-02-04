c256fccc634e2ae1c74dfb06c6afe582418a40ca
ymslark <ortakrenz@gmail.com>
2026-02-04 22:32:26 +0700

    remove semua gambar

--- connection.js
+++ connection.js
@@ -2,19 +2,26 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 const env = dotenv.config().parsed
+let connection = null
 
-const connection = () => {
-    mongoose.connect(env.MONGODB_URI, {
-        dbName: env.MONGODB_NAME,
-        user: env.MONGODB_USERNAME,
-        pass: env.MONGODB_PASSWORD
-    })
-    
+if(env.MONGODB_USERNAME === '' && env.MONGODB_PASSWORD === '') {
+    connection = () => {
+        mongoose.connect(env.MONGODB_URI, {
+            dbName: env.MONGODB_NAME,
+        })
+    }
+}else{
+    connection = () => {
+        mongoose.connect(env.MONGODB_URI, {
+            dbName: env.MONGODB_NAME,
+        })
+    }
+}
 const conn = mongoose.connection
 conn.on('error', console.error.bind(console, 'Connection Error :'))
 conn.once('open', () => {
     console.log('Connected to mongoDB!')
 })
-}
+
 
 export default connection
